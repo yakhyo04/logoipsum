c8c8a6a198cb3187d5ae16fd0c2cd5f3146b76f3
yakhyo04 <fenixrelaxation@gmail.com>
2021-11-05 21:18:33 +0500

    happened some changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,30 @@
+import React, {useState} from 'react';
 import './Header.css';
 import logoipsum from '../image/logoipsum.svg';
 
 function Header() {
-        const links = document.querySelector(".links");
+    const [toggleBtn, setToggleBtn] = useState(false)
     return (
         <header className="header">
             <div className="container">
             <ul>
                 <li>
                 <img src={logoipsum} alt="" />
-                <button class="nav-toggle" onClick={()=>links.classList.toggle("show-links")}>
+                <button class="nav-toggle" onClick={()=> setToggleBtn(!toggleBtn)}>
             <i class="fas fa-bars"></i>
           </button>
                 </li>
-                <li className="links">
+                {toggleBtn ?
+                <ul className="toggle__list">
+                    <li className="toggle__item"><a href="#">Home</a></li>
+                    <li className="toggle__item"><a href="#">Features</a></li>
+                    <li className="toggle__item"><a href="#">Pricing</a></li>
+                    <li className="toggle__item"><a href="#">Blog</a></li>
+                    <li><a src="#" className="header__link">Get Started</a></li>
+                </ul>
+                : null 
+            }
+            <li className="links">
                     <ul className="header__list">
                         <li className="header__item"><a href="#">Home</a></li>
                         <li className="header__item"><a href="#">Features</a></li>
@@ -21,7 +32,7 @@ function Header() {
                         <li className="header__item"><a href="#">Blog</a></li>
                     </ul>
                 </li>
-                <li className="links"><a href="#" className="header__link">Get Started</a></li>
+                <li className="links"><a href="#" className="header__link">Get Started</a></li> 
             </ul>
             </div>
         </header>
